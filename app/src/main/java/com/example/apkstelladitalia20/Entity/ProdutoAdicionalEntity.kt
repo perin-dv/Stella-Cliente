398738cffe763e5081a0ba2bd453c8eb9b20a003
Guilherme Perin <perin_gui_@hotmail.com>
2025-04-28 22:49:35 -0300

    ainda sem sucesso

---
+++ app/src/main/java/com/example/apkstelladitalia20/Entity/ProdutoAdicionalEntity.kt
@@ -0,0 +1,12 @@
+package com.example.apkstelladitalia20.Entity
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class ProdutoAdicionalEntity(
+    val id: String = "",
+    val nome: String = "",
+    val valor: Double = 0.0,
+    val imagemBase64: String? = null
+) : Parcelable
